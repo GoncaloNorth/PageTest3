e5012063649990dd09b7ae761c709bb49273428a
GoncaloNorth <goncalonortonm@gmail.com>
2025-05-27 22:19:07 +0100

    Update game.js

--- Bong/game.js
+++ Bong/game.js
@@ -1,4 +1,4 @@
-onst canvas = document.getElementById('gameCanvas');
+const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 const scoreElement = document.getElementById('score');
 
